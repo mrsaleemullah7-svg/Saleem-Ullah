05ebb8f6d658e7353520d22e3807029b1a339da6
Saleem Ullah <mr.saleemullah7@gmail.com>
2026-06-10 10:06:47 -0700

    conditional statement

---
+++ c.cpp1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n=45;
+	cout<<"Enter number:";
+	cin>>n;
+	if(n%2==0){
+		cout<<"Even number:";
+	}
+	else{
+		cout<<"Odd number:";
+	}
+	return 0;
+}
