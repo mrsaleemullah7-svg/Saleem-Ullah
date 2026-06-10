ea127475b270a38bcf171904ed15866696e94045
Saleem Ullah <mr.saleemullah7@gmail.com>
2026-06-10 10:07:48 -0700

    conditional statement

---
+++ c.cpp3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+	char ch;
+	cout<<"Enter ch:";
+	cin>>ch;
+	if(ch>='a'&&ch <='z'){
+		cout<<"Lowercass";
+	}else{
+	cout<<"	uppercass";
+	}
+	return 0;
+	
+	
+}
